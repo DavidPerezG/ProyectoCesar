c61586b1126841de4caf947642436e906460da36
DavidPerezGarcia <david.perez@sigob.com.mx>
2022-12-02 22:56:40 -0700

    cambios

---
+++ app/Http/Controllers/ReservasController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservas;
+use Illuminate\Http\Request;
+
+class ReservasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Reservas  $reservas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reservas $reservas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Reservas  $reservas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reservas $reservas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Reservas  $reservas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reservas $reservas)
+    {
+        //
+    }
+}
